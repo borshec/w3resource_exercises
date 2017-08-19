0644bac6bd11d385117def8274a2d9cb20b9f91e
borsh <young.specialist@gmail.com>
2017-08-19 20:16:50 +0300

    added 90.py

--- 90.py
+++ 90.py
@@ -1,2 +1,7 @@
 import sys
-print(sys.__name__)
+import pathlib
+import shutil
+psrc = pathlib.Path(sys.argv[0])
+# pdest = pathlib.Path(sys.argv[0]+"_")
+pdest2 = psrc.with_suffix(".copy")
+shutil.copyfile(psrc, pdest2)
